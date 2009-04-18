2adbf0217a7b9d25b6b5873346fd594673af8a28
Henrik Vendelbo <hvendelbo.dev@googlemail.com>
2009-04-18 21:16:11 +0100

    devonly cleanup

--- python/themaestro/__init__.py
+++ python/themaestro/__init__.py
@@ -2,19 +2,5 @@ import default_settings
 import theapps
 theapps.amend_default_settings(default_settings)
 
-def inject_devonly(settings):
-    from thepian.conf import structure
-    if structure.DEVELOPING:
-        print 'Enabling devonly development'
-        if 'devonly' not in settings.INSTALLED_APPS:
-            settings.INSTALLED_APPS = list(settings.INSTALLED_APPS) + ['devonly']
-        if not hasattr(settings,'URLCONFS'):            
-            settings.URLCONFS = { 'www': settings.ROOT_URLCONF }
-        if 'media' not in settings.URLCONFS:
-            settings.URLCONFS['media'] = 'devonly.media_urls'
-        import django.views.debug
-        import theapps.supervisor.debug
-        django.views.debug.technical_404_response = theapps.supervisor.debug.technical_404_response
-   
 
     
