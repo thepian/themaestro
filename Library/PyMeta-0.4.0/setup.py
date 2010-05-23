4061796600ae1cf5bac60dd5c072242f38009112
Thepian <thepian@thepia.com>
2010-05-23 12:49:27 +0100

    PyMeta 0.4

---
+++ Library/PyMeta-0.4.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Setup script for the PyMeta distribution."""
+from distutils.core import setup
+setup(
+    name = "PyMeta",
+    version = "0.4.0",
+    url = "http://launchpad.net/pymeta",
+    description = "Pattern-matching language based on OMeta",
+    author = "Allen Short",
+    author_email = "washort42@gmail.com",
+    license = "MIT License",
+    packages = ["pymeta"]
+    )
