812c3f395a6c309b96904a768a7f5fdd095b236b
Henrik Vendelbo <thepian@thepia.com>
2010-06-10 10:57:29 +0100

    static urls for mediasite

--- Library/maestro/mediaserver/urls.py
+++ Library/maestro/mediaserver/urls.py
@@ -1,7 +1,13 @@
+from thepian.conf import structure
 from handlers import *
+from tornado.web import StaticFileHandler
 
 urls = [
-    (r"/", HomeHandler),
+    (r"^/$", HomeHandler),
+    (r"/static/(.*)", StaticFileHandler, {"path": structure.MEDIASITE_DIRS[0]}),
+    (r"/960gs/(.*)", StaticFileHandler, {"path": structure.MEDIASITE_DIRS[0] + "/960gs"}),
+    (r"/images/(.*)", StaticFileHandler, {"path": structure.MEDIASITE_DIRS[0] + "/images"}),
+    (r"/demo/(.*)", StaticFileHandler, {"path": structure.MEDIASITE_DIRS[0] + "/demo"}),
     (r"/css/(\w+\.css)", CssHandler),
     (r"/js/(\w+\.js)/verify/(.*)", JsVerifyHandler),
     (r"/js/(\w+)/(\w+\.js)/verify/(.*)", JsVerifyDetailHandler),
