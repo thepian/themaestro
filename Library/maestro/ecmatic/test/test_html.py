f5d799e116d18053efe64b8ce7fa42a7012cd906
Henrik Vendelbo <thepian@thepia.com>
2010-12-23 07:41:14 +0100

    simple html parser test still missing single elements, doctype, boolean attributes, unquoted attributes

--- Library/maestro/ecmatic/test/test_html.py
+++ Library/maestro/ecmatic/test/test_html.py
@@ -6,44 +6,34 @@ import unittest
 html_grammar = r"""
 hspace    = ' ' | '\t'
 vspace    = '\n'
-optspace  = ?(self.parenthesis) (hspace | '\\'? vspace | comment)* | (hspace | '\\' vspace)* comment?
-mandspace = ?(self.parenthesis) (hspace | '\\'? vspace | comment)+ | (hspace | '\\' vspace)+ comment?
 
-comment  = '#' line_rest:c -> ['comment', c]
-
-name_start = letter | '$' | '_'
+name_start = letter | '?' | '!'
 name_rest = name_start | digit
 iname = name_start:s name_rest*:r -> s + ''.join(r)
 isreserved :x = ?(self.is_reserved(x))
-name = iname:n ~isreserved(n)  -> ['name',n]
+name = iname:n ~isreserved(n)  -> n
 
-escaped_char = '\\' :x -> ('\\' + x).decode('string_escape')
-string3 :e = match_string(e) (escaped_char | ~(?(len(e) != 3) vspace | match_string(e)) anything)*:c match_string(e) -> ''.join(c)
-string2 = string3('\"\"\"') | string3("\'\'\'") | string3('"') | string3("'")
-string = (string2:s optspace -> s)+:s -> ['string', ''.join(s)]
+string3 :e = match_string(e) (~(match_string(e)) anything)*:c match_string(e) -> ''.join(c)
+string2 = string3('"') | string3("'")
+string = (string2:s -> s)+:s -> ''.join(s)
 
-tag = '<' name:n '>' html:c '<' '/' token:n '>'
-html = tag*
-"""
-
-"""
-tag = '<' spaces name:n spaces attribute*:attrs '>' html:c '<' '/' token:n spaces '>'
+tag = '<' spaces name:n spaces attribute*:attrs '>' html:c '<' '/' name:n spaces '>'
         -> [n.lower(), dict(attrs), c]
 
 html = (text | tag)*
 
 text = (~('<') anything)+:t -> ''.join(t)
 
-attribute = spaces name:k '=' quotedString:v -> (k, v)
-
-quotedString = (('"' | '\''):q (~exactly:q anything)*:xs exactly:q
-                     -> ''.join(xs))
+string_attribute = name:k '=' string:v -> (k, v)
+bool_attribute = name:k (~('=') anything)? -> (k,True)
+attribute = spaces (string_attribute | bool_attribute)  
+attributes = spaces attribute*:attrs spaces -> dict(attrs)
 """
 
 def p(s):
     print s
     
-class Grammar(OMeta.makeGrammar(html_grammar, {'p': p})):
+class Grammar(OMeta.makeGrammar(html_grammar, {'p': p}, name="HTML")):
     def is_reserved(self,name):
         "Is the attribute name reserved"
         return False
@@ -51,10 +41,58 @@ class Grammar(OMeta.makeGrammar(html_grammar, {'p': p})):
 
 class GrammarTestCase(unittest.TestCase):
     
-    def test_makeGrammar(self):
-        g = Grammar("<html><body></body></html>")
-        print g.apply("html")
-        # self.assertEqual(g.apply("num")[0], 314159)
-        # self.assertNotEqual(len(results), 0)
+    def test_whitespace(self):
+        g = Grammar("<html> </html>")
+        result,error = g.apply("html")
+        assert result == [["html", {}, [" "]]]
 
-    
+        g = Grammar(" <html> </html>")
+        result,error = g.apply("html")
+        assert result == [" ",["html", {}, [" "]]]
+
+        g = Grammar("""
+   <html> </html>""")
+        result,error = g.apply("html")
+        assert result == ["\n   ",["html", {}, [" "]]]
+
+    def test_html_language(self):
+        g = Grammar("<html language='en'> </html>")
+        result,error = g.apply("html")
+        assert result == [["html", { 'language':'en' }, [" "]]]
+
+    def test_string_attr(self):
+        g = Grammar('a="b"')
+        result, error = g.apply("string_attribute")
+        assert result == ("a","b") 
+
+        g = Grammar('a="b" c="d"')
+        result, error = g.apply("attributes")
+        assert result == { "a":"b", "c":"d" } 
+
+    def test_bool_attr(self):
+        g = Grammar('a')
+        result, error = g.apply("bool_attribute")
+        assert result == ("a",True) 
+
+        g = Grammar('a c')
+        result, error = g.apply("attributes")
+        assert result == { "a":True, "c":True } 
+
+        g = Grammar("""\
+<html><body><input disabled ></input></body></html>
+""")
+        result,error = g.apply("html")
+        print error
+        assert result == [
+            ["html", {}, [
+                ['body', {}, [['input', { "disabled":True }, [] ]]]
+                ]],
+            '\n']
+
+    def notest_doctype(self):
+         g = Grammar("""\
+<|DOCTYPE html>
+<html> </html>""")
+         result,error = g.apply("html")
+         assert result == [["html", {}, [" "]]]
+        
