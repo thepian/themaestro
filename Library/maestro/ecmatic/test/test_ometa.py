1fc2eb7413b4ead46b72912876e674435a2ad62b
Henrik Vendelbo <thepian@thepia.com>
2010-12-25 23:32:01 +0100

    read ahead attribute semantic

--- Library/maestro/ecmatic/test/test_ometa.py
+++ Library/maestro/ecmatic/test/test_ometa.py
@@ -10,19 +10,24 @@ class GrammarTestCase(unittest.TestCase):
     
     def test_constant_bits(self):
         Grammar = OMeta.makeGrammar(r"""
-var = 'v' 'a' 'r' spaces letter+:name ';' -> ['var', ''.join(name)]
+var = "var" spaces letter+:name ';' -> ['var', ''.join(name)]
 """, {})
         g = Grammar("var myvar;")
         result,error = g.apply("var")
         assert result == ['var', 'myvar']
         
-    def no_test_read_ahead(self):
+    def test_read_ahead(self):
         Grammar = OMeta.makeGrammar(r"""
+attributes = (attribute:a ' ')+ -> a
 attribute = bool | valued
-bool = (letter+):name &(anything ~('=')) -> [name,True]
-valued = (letter+):name '=' (letter+):value -> [name,value]
+bool = (letter+):name ~('=') -> (''.join(name),True)
+valued = (letter+):name '=' (letter+):value -> (''.join(name),''.join(value))
 """, {})
 
+        g = Grammar("a")
+        result,error = g.apply("attribute")
+        assert result == ('a',True)
+
         g = Grammar("a=b")
-        result,error = g.apply("valued")
+        result,error = g.apply("attribute")
         assert result == ('a','b')
