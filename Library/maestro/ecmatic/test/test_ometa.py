3e450ae3424be8dcead72e7be9e19d1710f50784
Henrik Vendelbo <thepian@thepia.com>
2010-12-26 06:41:01 +0100

    test optional match and nested productions

--- Library/maestro/ecmatic/test/test_ometa.py
+++ Library/maestro/ecmatic/test/test_ometa.py
@@ -18,7 +18,6 @@ var = "var" spaces letter+:name ';' -> ['var', ''.join(name)]
         
     def test_read_ahead(self):
         Grammar = OMeta.makeGrammar(r"""
-attributes = (attribute:a ' ')+ -> a
 attribute = bool | valued
 bool = (letter+):name ~('=') -> (''.join(name),True)
 valued = (letter+):name '=' (letter+):value -> (''.join(name),''.join(value))
@@ -31,3 +30,37 @@ valued = (letter+):name '=' (letter+):value -> (''.join(name),''.join(value))
         g = Grammar("a=b")
         result,error = g.apply("attribute")
         assert result == ('a','b')
+
+    def test_nested_productions(self):
+        Grammar = OMeta.makeGrammar(r"""
+attributes = spaces (attribute:a spaces -> a)+:as -> as
+attribute = bool | valued
+bool = (letter+):name ~('=') -> (''.join(name),True)
+valued = (letter+):name '=' (letter+):value -> (''.join(name),''.join(value))
+""", {})
+
+        g = Grammar("a=b")
+        result,error = g.apply("attributes")
+        assert result == [('a','b')]
+
+        g = Grammar("a=b c d=e")
+        result,error = g.apply("attributes")
+        assert result == [('a','b'),('c',True),('d','e')]
+
+
+    def test_optional(self):
+        Grammar = OMeta.makeGrammar(r"""
+word_comment = "/*" ' '? letter*:t ' '? "*/" -> ['comment', ''.join(t)]
+""", {})
+        result,error = Grammar("/* abc */").apply("word_comment")
+        assert result == ['comment', 'abc']
+        
+        result,error = Grammar("/*abc*/").apply("word_comment")
+        assert result == ['comment', 'abc']
+
+        result,error = Grammar("/* */").apply("word_comment")
+        assert result == ['comment', '']
+
+        result,error = Grammar("/**/").apply("word_comment")
+        assert result == ['comment', '']
+        
