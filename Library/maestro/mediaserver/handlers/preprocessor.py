9814b3c0cd987aac093318349d9856fecd96e545
Henrik Vendelbo <thepian@thepia.com>
2011-07-17 13:31:52 -0600

    Using regular redis instead

--- Library/maestro/mediaserver/handlers/preprocessor.py
+++ Library/maestro/mediaserver/handlers/preprocessor.py
@@ -10,26 +10,33 @@ REDIS_PORT = 6379
 
 r = redis.Redis(REDIS_HOST, REDIS_PORT, db=9)
 
-c = brukva.Client(REDIS_HOST, REDIS_PORT)
-c.connect()
-c.select(9)
+# c = brukva.Client(REDIS_HOST, REDIS_PORT)
+# c.connect()
+# c.select(9)
 
-c.set('account/hash.js', 'aaaaaaa')
-c.set('foo2', 'bar2')
+r.set('account/hash.js', 'aaaaaaa')
+# c.set('foo2', 'bar2')
 
 # print c.get('%s/%s.js' % ("account","hash"))
 
 class JsPreProcessHandler(tornado.web.RequestHandler):
     
-    @tornado.web.asynchronous
-    @brukva.adisp.process
+    # @tornado.web.asynchronous
+    # @brukva.adisp.process
     def get(self, account, hash):
         try:
-            tx = yield c.async.get('%s/%s.js' % (account,hash))
-            
-            self.set_header('Content-Type', 'text/javascript')
-            self.write(tx)
-            self.finish()
+            key = '%s/%s.js' % (account,hash)
+            # tx = yield c.async.get(key)
+            tx = r.get(key)
+            if not tx:
+                print 'no entry for ', key
+                self.send_error(error_code=404)
+                self.finish()
+                # raise tornado.web.HTTPError(404)
+            else:
+                self.set_header('Content-Type', 'text/javascript')
+                self.write(tx)
+                self.finish()
             return 
         except Exception, e:
             print e
